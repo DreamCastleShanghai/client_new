aa6937f0e527fbdf5094af41a4755bdec51c0a08
alizwl <csbuss@163.com>
2016-02-16 15:17:53 +0800

    android push

--- Classes/FNoticeManager.cpp
+++ Classes/FNoticeManager.cpp
@@ -20,7 +20,7 @@ FNoticeManager::FNoticeManager()
     
     int ret = 0;
 
-    const char *sql_createtable = "CREATE TABLE IF NOT EXISTS notice(id int auto_increment, sid int, title VARCHAR(256), readed int, start_time int, end_time int);";
+    const char *sql_createtable = "CREATE TABLE IF NOT EXISTS notice(id int auto_increment, sid int, title VARCHAR(256), readed int, start_time int64, end_time int64);";
 	sqlite3_stmt *stmt;
     int ok=sqlite3_prepare_v2(RootWindow::getInstance()->getSqlite3(), sql_createtable, -1, &stmt, NULL);
 	ok |= sqlite3_step(stmt);
@@ -53,15 +53,15 @@ FNoticeManager::~FNoticeManager()
 
 bool FNoticeManager::addNotice(int sid, int type, std::string &title, time_t start, time_t end)
 {
-    CADevice::sendLocalNotification("session", title.c_str(), start - getTimeSecond() - PRETIME, crossapp_format_string("%d", sid).c_str());
-    
     deleteNotice(sid);
     
+    CADevice::sendLocalNotification("session", title.c_str(), start - getTimeSecond() - PRETIME, crossapp_format_string("%d", sid).c_str());
+
     int ok = sqlite3_bind_text(_sqlite_stmt_add, 1, crossapp_format_string("%d", sid).c_str(), -1, SQLITE_TRANSIENT);
     ok |= sqlite3_bind_text(_sqlite_stmt_add, 2, title.c_str(), -1, SQLITE_TRANSIENT);
     ok |= sqlite3_bind_text(_sqlite_stmt_add, 3, crossapp_format_string("%d", 0).c_str(), -1, SQLITE_TRANSIENT);
-    ok |= sqlite3_bind_text(_sqlite_stmt_add, 4, crossapp_format_string("%d", start).c_str(), -1, SQLITE_TRANSIENT);
-    ok |= sqlite3_bind_text(_sqlite_stmt_add, 4, crossapp_format_string("%d", end).c_str(), -1, SQLITE_TRANSIENT);
+    ok |= sqlite3_bind_text(_sqlite_stmt_add, 4, crossapp_format_string("%ld", start).c_str(), -1, SQLITE_TRANSIENT);
+    ok |= sqlite3_bind_text(_sqlite_stmt_add, 5, crossapp_format_string("%ld", end).c_str(), -1, SQLITE_TRANSIENT);
     
     ok |= sqlite3_step(_sqlite_stmt_add);
     ok |= sqlite3_reset(_sqlite_stmt_add);
@@ -100,8 +100,8 @@ std::vector<FNotice>& FNoticeManager::getNotices()
         notice.sid = sqlite3_column_int(_sqlite_stmt_get, 0);
         notice.title = crossapp_format_string("%s", sqlite3_column_text(_sqlite_stmt_get, 1));
         notice.readed = sqlite3_column_int(_sqlite_stmt_get, 2);
-        notice.startTime = sqlite3_column_int(_sqlite_stmt_get, 3);
-        notice.endTime = sqlite3_column_int(_sqlite_stmt_get, 4);
+        notice.startTime = sqlite3_column_int64(_sqlite_stmt_get, 3);
+        notice.endTime = sqlite3_column_int64(_sqlite_stmt_get, 4);
         m_notices.push_back(notice);
     }
     ok |= sqlite3_reset(_sqlite_stmt_get);
